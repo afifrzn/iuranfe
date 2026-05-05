38a872f2099e2180d378a9c74e3d8586c59d83c1
Afif-1002 <afifrahmanzian02@gmail.com>
2026-05-05 18:03:48 +0700

    fix app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-const API = import.meta.env.VITE_API_URL
+const API = import.meta.env.VITE_API_URL || 'http://10.10.101.243'
 
 const data = ref([])
 const total = ref(0)
@@ -10,9 +10,13 @@ const name = ref('')
 const withMeal = ref(false)
 
 const fetchData = async () => {
-  const res = await axios.get(`${API}/participants`)
-  data.value = res.data.data
-  total.value = res.data.total
+  try {
+    const res = await axios.get(`${API}/participants`)
+    data.value = res.data.data
+    total.value = res.data.total
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 const add = async () => {
@@ -31,3 +35,25 @@ const upgrade = async (id) => {
 
 onMounted(fetchData)
 </script>
+
+<template>
+  <div style="padding: 20px">
+    <h1>Urunan Villa</h1>
+
+    <input v-model="name" placeholder="Nama" />
+    <label>
+      <input type="checkbox" v-model="withMeal" />
+      Pakai Makan
+    </label>
+    <button @click="add">Tambah</button>
+
+    <ul>
+      <li v-for="p in data" :key="p.id">
+        {{ p.name }} - {{ p.amount }}
+        <button @click="upgrade(p.id)">Upgrade</button>
+      </li>
+    </ul>
+
+    <h2>Total: {{ total }}</h2>
+  </div>
+</template>
